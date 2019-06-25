44d6b799ab08abef2adfc629d3131e0e9d9de1ca
Alexander Le <alexanderle0817@gmail.com>
2019-06-25 19:21:03 -0400

    implemented calculateTotalSalary function

--- lineup.js
+++ lineup.js
@@ -1,5 +1,7 @@
 export function calculateTotalSalary(lineup) {
-  return 0
+  return lineup.reduce( (salaryCount, player) => {
+    return salaryCount + player.salary 
+  }, 0)
 }
 
 export function getPositionCounts(lineup) {
